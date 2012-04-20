1732b847200069ccbdd1dee477c06cbd84ce956d
unknown <jason.a.gray@MW78029785Z4RG.dir.svc.accenture.com>
2012-04-20 16:13:15 -0400

    Added test functionality to the log button

--- src/org/SWTORparser/Gui/MainController.java
+++ src/org/SWTORparser/Gui/MainController.java
@@ -6,6 +6,8 @@ package org.SWTORparser.Gui;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 
 /**
@@ -18,6 +20,11 @@ public class MainController implements Initializable  {
 	public void initialize(URL arg0, ResourceBundle arg1) {
 		// TODO Auto-generated method stub
 		
+	}
+	
+	@FXML
+	private void buttonLogLocation(final ActionEvent event){
+		System.out.println("Hello World");
 	}
 
 }
